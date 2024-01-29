4831ef8e2b9057f35cefd685927bf91cf5dbbef6
bernixCodes <adjoa.bernie@gmail.com>
2024-01-29 09:42:45 +0000

    labs endpoints

--- lab_backend/server.js
+++ lab_backend/server.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/api/drugs", require("./routes/drugRoutes"));
+app.use("/api/labs", require("./routes/labRoutes"));
 app.use(errorHandler);
 
 app.listen(port, () => {
